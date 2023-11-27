55d788e688f55d90282b3e5076f9c0085e1af8e4
Om Chiplunkar <chiplunkarom10@gmail.com>
2023-11-27 16:44:59 +0530

    project finished

---
+++ app/src/main/java/com/example/project_final_year/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.project_final_year
+
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.frame_new_case_register)
+
+
+
+    }
+}
